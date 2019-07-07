209bdb3ff1258d2ede0bdf769f364358de346d81
Starboy-Sharma <pankajsharmavats88@gmail.com>
2019-07-07 16:06:02 +0530

    Create Stopwatch with OOP JS

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-// Constructor method
-function Circle(radius) {
-  this.radius = radius;
+// Soptwatch in JS 😊
+function Soptwatch() {
 
-  let defaultLocation = {x:0, y:0};
+   let startTime, endTime, running, duration = 0;
 
-  let computeOptimumLocation = function (factory) { // this method is a private property and circle object can not access
-    // ...
-    console.log("My scope is temporary");
-  }
+   this.start = function () {
+      if(running)
+        throw new Error("Stop watch is already runing");
 
-  this.draw = function() {
+      running = true;
+
+      startTime = new Date();
+   };
+
+   this.stop = function () {
+    
+      if(!running)
+        throw new Error("Stop watch is not runing");
+
+      running = false;
+
+      endTime = new Date();
+
+      const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+      duration += seconds;
+  };
+
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
 
 
-    computeOptimumLocation(3.2);
-    console.log('draw');
-  }
+  // Getter & Setters
+  Object.defineProperty(this, 'duration', {
+    get: function() { return duration; }
+  });
 }
 
 
-// create an object
-
-const circle = new Circle(2.0);
-circle.draw();
-
-// Want to Know About Closure and Scopes Visit here: https://stackoverflow.com/questions/111102/how-do-javascript-closures-work
+// create our Stopwatch object
+const SW = new Soptwatch();
